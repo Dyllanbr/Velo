2410d7376fbf993b6a70d1e6341b06fe809def7c
Dyllanb <brendondyllan5@gmail.com>
2026-03-25 22:26:14 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,24 +1,18 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert  
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //arrange
   await page.goto('http://localhost:5173/')
-
-  //checkpoint 1: verificar se a página de consulta de pedidos está visível
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-  // Acessar a página de consulta de pedidos
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  //checkpoint 2: verificar se a página de consulta de pedidos está visível
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-
+  //act
   await page.getByTestId('search-order-id').fill('VLO-HXX69M')
-
   await page.getByTestId('search-order-button').click()
-  
-  await page.getByTestId('order-result-id').click()
 
+  //assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-HXX69M')
 
